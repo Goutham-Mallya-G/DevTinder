7eb383eca8ac17c0a70dc48fed5b111e76513535
Goutham Mallya G <gouthammallya2@gmail.com>
2025-02-11 20:29:45 +0530

    added middle wares

---
+++ src/middlewares/app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express();
+const {adminAuth,userAuth} = require("../../Authentication/auth.js")
+
+app.use("/admin",adminAuth);
+
+app.get("/user/login",(req,res,next)=>{
+    res.send("You are redirecting to login page")
+    next
+})
+
+app.get("/user/data",userAuth,(req,res,next)=>{
+    res.send("This is the profile data")
+})
+
+app.get("/admin/getAllUser",(req,res,next)=>{
+    res.send("This is information about all users")
+})
+
+app.get("/admin/deleteUser",(req,res,next)=>{
+    res.send("You have deleted a user")
+})
+
+app.listen(3001);
