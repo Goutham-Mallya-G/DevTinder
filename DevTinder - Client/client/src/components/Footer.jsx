48cbd31fcea3391d2d1c776405f88dcfaef641be
Goutham-Mallya-G <gouthammallya2@gmail.com>
2025-08-07 22:30:51 +0530

    Patch : Footer

--- DevTinder - Client/client/src/components/Footer.jsx
+++ DevTinder - Client/client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 w-full">
   <aside>
-    <p className="text-center text-sm">Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+    <p className="text-center text-sm">Copyright © {new Date().getFullYear()} - Goutham Mallya G</p>
   </aside>
 </footer>
   )
